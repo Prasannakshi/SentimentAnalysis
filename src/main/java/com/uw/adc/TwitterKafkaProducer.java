2f7e4ff61421bf629654a61ae2b577289d7704dc
UW-SG <gptsonam@gmail.comgit config --global user.email gptsonam@gmail.com>
2016-12-03 20:40:57 -0800

    Kafka Producer

---
+++ src/main/java/com/uw/adc/TwitterKafkaProducer.java
@@ -0,0 +1,84 @@
+package com.uw.adc;
+
+import java.util.Properties;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import kafka.producer.KeyedMessage;
+import kafka.producer.ProducerConfig;
+
+import com.google.common.collect.Lists;
+import com.twitter.hbc.ClientBuilder;
+import com.twitter.hbc.core.Client;
+import com.twitter.hbc.core.Constants;
+import com.twitter.hbc.core.endpoint.StatusesFilterEndpoint;
+import com.twitter.hbc.core.processor.StringDelimitedProcessor;
+import com.twitter.hbc.httpclient.auth.Authentication;
+import com.twitter.hbc.httpclient.auth.OAuth1;
+
+/*
+ * Created by Sonam Gupta 
+ */
+
+
+public class TwitterKafkaProducer {
+
+	private static final String topic = "twitter-topic";
+
+	public static void run(String consumerKey, String consumerSecret,
+			String token, String secret) throws InterruptedException {
+
+		Properties properties = new Properties();
+		properties.put("metadata.broker.list", "localhost:9092");
+		properties.put("serializer.class", "kafka.serializer.StringEncoder");
+		properties.put("client.id","camus");
+		ProducerConfig producerConfig = new ProducerConfig(properties);
+		kafka.javaapi.producer.Producer<String, String> producer = new kafka.javaapi.producer.Producer<String, String>(
+				producerConfig);
+
+		BlockingQueue<String> queue = new LinkedBlockingQueue<String>(10000);
+		StatusesFilterEndpoint endpoint = new StatusesFilterEndpoint();
+		// add some track terms
+		endpoint.trackTerms(Lists.newArrayList("twitterapi",
+				"#joke"));
+
+		Authentication auth = new OAuth1(consumerKey, consumerSecret, token,
+				secret);
+		
+		Client client = new ClientBuilder().hosts(Constants.STREAM_HOST)
+				.endpoint(endpoint).authentication(auth)
+				.processor(new StringDelimitedProcessor(queue)).build();
+
+		// Establish a connection
+		client.connect();
+
+		// Do whatever needs to be done with messages
+		for (int msgRead = 0; msgRead < 1000; msgRead++) {
+			KeyedMessage<String, String> message = null;
+			try {
+				message = new KeyedMessage<String, String>(topic, queue.take());
+				System.out.println(message);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			producer.send(message);
+		}
+		producer.close();
+		client.stop();
+
+	}
+
+	public static void main(String[] args) {
+		try {
+			String ConsumerKey = "dummy";
+			String ConsumerSecret = "dummy";
+
+			String AccessToken	= "dummy";
+			String AccessTokenSecret = "dummy";
+			
+			TwitterKafkaProducer.run(ConsumerKey, ConsumerSecret , AccessToken, AccessTokenSecret);
+		} catch (InterruptedException e) {
+			System.out.println(e);
+		}
+	}
+}
